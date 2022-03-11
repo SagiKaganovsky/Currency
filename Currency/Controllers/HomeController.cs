aff303b21146a187f519e9e9d9505b78adb6d1a3
Sergei Kaganovsky <sergkagan@gmail.com>
2022-03-11 11:14:42 +0200

    Add project files.

---
+++ Currency/Controllers/HomeController.cs
@@ -0,0 +1,63 @@
+﻿using Currency.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Currency.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public HomeController(ILogger<HomeController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        { 
+            return View();
+        }
+
+        public async Task<string> Currencies()
+        {
+            var httpRequestMessage = new HttpRequestMessage(
+            HttpMethod.Get,
+            "https://www.boi.org.il/currency.xml");
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+            string json = string.Empty;
+            if (httpResponseMessage.IsSuccessStatusCode)
+            {
+                var contentStream = await httpResponseMessage.Content.ReadAsStringAsync();
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(contentStream);
+                json = JsonConvert.SerializeXmlNode(doc);
+            }
+            return json; 
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Currency/wwwroot/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+$.ajax({
+    url: '/Home/Currencies',
+    type: 'GET',
+    dataType: 'json',
+    success: function (data) {
+        const filteredCurrencies = data.CURRENCIES.CURRENCY.filter((curr) => Number(curr.CHANGE) < 0);
+        let table = new Tabulator('#currencies-table', {
+            data: filteredCurrencies,
+            autoColumns:true
+        })
+    },
+    error: function (request, error) {
+        alert("Request: " + JSON.stringify(request));
+    }
+});
